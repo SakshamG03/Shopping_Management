b739daa7f35a1f5c660a0e47fa24784be3c219b4
Saksham <161133251+SakshamG03@users.noreply.github.com>
2026-02-15 15:46:26 +0530

    Delete Code 1.py

--- Code 1.py
+++
@@ -1,9 +0,0 @@
-# These are the instructions for the project work.
-#
-# TOPIC: GROCERY MANAGEMENT SYSTEM\
-#
-# Day 1 Target:
-# Watch four videos on Grocery Management Project from the Playlist
-
-# Know the basic idea of the project and review the changes
-#
